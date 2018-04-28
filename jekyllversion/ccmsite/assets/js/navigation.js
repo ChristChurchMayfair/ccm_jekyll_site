ac9f673807b7fb00b20e762e472fb41bd057121f
Tom Duckering <tom.duckering@gmail.com>
2018-04-28 16:35:03 +0100

    Adding address and directions
    Directions button only shows if javascript is enabled.

--- jekyllversion/ccmsite/assets/js/navigation.js
+++ jekyllversion/ccmsite/assets/js/navigation.js
@@ -1,14 +1,22 @@
 function mapsSelector() {
   var latitude = 51.505263;
   var longitude = -0.148704;
+  var isMac = navigator.platform.toUpperCase().indexOf('MAC')>=0;
   if /* if we're on iOS, open in Apple Maps */
     ((navigator.platform.indexOf("iPhone") != -1) ||
      (navigator.platform.indexOf("iPad") != -1) ||
      (navigator.platform.indexOf("iPod") != -1))
     window.open(`maps://maps.google.com/maps?daddr=${latitude},${longitude}&amp;ll=`);
-else /* else use Google */
+  // else if (isMac) {
+  //   window.open(`maps://maps.google.com/maps?daddr=${latitude},${longitude}&amp;ll=`,'_self');
+  // }
+  else {/* else use Google */
     window.open(`https://maps.google.com/maps?daddr=${latitude},${longitude}&amp;ll=`);
+  }
 }
 
-var navigationLink = document.getElementById('directions-link');
-navigationLink.onclick = mapsSelector;
+var directions = document.getElementById("directions");
+directions.style.display = "block"
+
+var directionsLink = document.getElementById('directions-link');
+directionsLink.onclick = mapsSelector;
