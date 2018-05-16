88774f6ba4af1b05dcc48d855bc417be368c4089
Tom Duckering <tom.duckering@gmail.com>
2018-05-16 17:18:45 +0100

    Added shonky menu reveal
    Needs tuning to be less awful.

--- jekyllversion/ccmsite/assets/javascript/menu.js
+++ jekyllversion/ccmsite/assets/javascript/menu.js
@@ -5,12 +5,18 @@ function openMenuButtonClicked() {
 
 
   var menu = document.getElementById("menu");
-  menu.style.display = "grid";
+  //menu.style.display = "grid";
+  menu.style.height = "90vh";
+  menu.style.paddingTop = "15px";
+  menu.style.paddingBottom = "15px";
 }
 
 function closeMenuButtonClicked() {
   var menu = document.getElementById("menu");
-  menu.style.display = "none";
+  //menu.style.display = "none";
+  menu.style.height = "0px";
+  menu.style.paddingTop = "0px";
+  menu.style.paddingBottom = "0px";
 
   var header = document.getElementById("header");
   header.style.display = "grid";
