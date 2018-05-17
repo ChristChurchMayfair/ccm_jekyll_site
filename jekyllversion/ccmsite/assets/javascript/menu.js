1142ea14c47a09d15a57c235a71cfd7e82a66f16
Tom Duckering <tom.duckering@gmail.com>
2018-05-17 10:30:58 +0100

    Trying out menu animation with CSS keyframes...
    Seems to have some quirks with the need to reset the animation.

--- jekyllversion/ccmsite/assets/javascript/menu.js
+++ jekyllversion/ccmsite/assets/javascript/menu.js
@@ -1,33 +1,31 @@
 function openMenuButtonClicked() {
 
   var header = document.getElementById("header");
-  header.style.display = "none";
+  //header.style.display = "none";
 
 
   var menu = document.getElementById("menu");
-  //menu.style.display = "grid";
-  menu.style.height = "90vh";
-  menu.style.paddingTop = "15px";
-  menu.style.paddingBottom = "15px";
+
+  menu.style.animationName = "navOpen";
+  //menu.style.animationPlayState = "running";
 }
 
 function closeMenuButtonClicked() {
   var menu = document.getElementById("menu");
-  //menu.style.display = "none";
-  menu.style.height = "0px";
-  menu.style.paddingTop = "0px";
-  menu.style.paddingBottom = "0px";
+  menu.style.animationName = "navOpen";
+  //menu.style.animationPlayState = "running";
+  menu.style.animationDirection = "reverse";
 
   var header = document.getElementById("header");
   header.style.display = "grid";
 }
 
 function addHandlerToMenuButtons() {
-var openMenuButton = document.getElementById("open-menu-button");
-openMenuButton.onclick = openMenuButtonClicked;
+  var openMenuButton = document.getElementById("open-menu-button");
+  openMenuButton.onclick = openMenuButtonClicked;
 
-var closeMenuButton = document.getElementById("close-menu-button");
-closeMenuButton.onclick = closeMenuButtonClicked;
+  var closeMenuButton = document.getElementById("close-menu-button");
+  closeMenuButton.onclick = closeMenuButtonClicked;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
