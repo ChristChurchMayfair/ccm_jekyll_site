a6ebb79663357bbb6b643417c74ac07e070f80d6
Tom Duckering <tom.duckering@gmail.com>
2018-06-30 15:46:10 -0700

    Pulling talksdata from graphcool

--- assets/javascript/talks.js
+++ assets/javascript/talks.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function showPodcastLink() {
   var isMac = navigator.platform.toUpperCase().indexOf('MAC')>=0;
   if /* if we're on iOS, show the podcast links */
@@ -23,17 +24,17 @@ function addSermonSeries(sermon_data) {
   var sermonTemplate = document.getElementById('sermon_template');
   var series;
   
-  for (series of sermon_data.series) {
+  for (series of sermon_data.data.allSeries) {
 
-    var sermonsForSeries = sermon_data.episodes.filter(sermon => sermon.seriesId == series.id)
+    var sermonsForSeries = series.sermons
 
     var sermonSeriesDiv = document.importNode(sermonSeriesTemplate.content, true);
     
 
-    sermonSeriesDiv.querySelector(".series-title").textContent = series.title;
-    sermonSeriesDiv.querySelector(".series-book").textContent = series.mainBook;
+    sermonSeriesDiv.querySelector(".series-title").textContent = series.name;
+    sermonSeriesDiv.querySelector(".series-book").textContent = "No Book"
     //sermonSeriesDiv.querySelector(".series-service").textContent = series.service;
-    sermonSeriesDiv.querySelector(".series-graphic").setAttribute("style",`background-image: url(${series.imageURL});`);
+    sermonSeriesDiv.querySelector(".series-graphic").setAttribute("style",`background-image: url(${series.image3x2Url});`);
 
     var sermonList = sermonSeriesDiv.querySelector(".sermon-list");
     
@@ -41,14 +42,14 @@ function addSermonSeries(sermon_data) {
     for (sermon of sermonsForSeries) {
       var sermonDiv = document.importNode(sermonTemplate.content, true);
 
-      var dateString = new Date(Date.parse(sermon.date)).toLocaleDateString();
+      var dateString = new Date(Date.parse(sermon.preachedAt)).toLocaleDateString();
 
-      sermonDiv.querySelector(".sermon-title").textContent = sermon.title;
-      sermonDiv.querySelector(".sermon-speaker").textContent = sermon.speaker;
+      sermonDiv.querySelector(".sermon-title").textContent = sermon.name;
+      sermonDiv.querySelector(".sermon-speaker").textContent = sermon.speakers[0].name;
       sermonDiv.querySelector(".sermon-passage").textContent = sermon.passage;
       sermonDiv.querySelector(".sermon-date").textContent = dateString;
-      sermonDiv.querySelector(".sermon-download").setAttribute("href",sermon.audioFileURL);
-      sermonDiv.querySelector(".sermon-player").setAttribute("src",sermon.audioFileURL);
+      sermonDiv.querySelector(".sermon-download").setAttribute("href",sermon.url);
+      sermonDiv.querySelector(".sermon-player").setAttribute("src",sermon.url);
 
       sermonList.appendChild(sermonDiv);
     }
@@ -56,28 +57,48 @@ function addSermonSeries(sermon_data) {
     sermonSeriesSection.appendChild(sermonSeriesDiv);
   }
 }
-
-//Location of the sermon data.
-var sermon_data_url = "http://ccmtalkstest.s3-website-eu-west-1.amazonaws.com/sermondata.json";
-if (document.URL.includes("localhost")) {
-  sermon_data_url = "/fakesermondata.json";
+function queryGraphCool(url, query) {
+  // Default options are marked with *
+  return fetch(url, {
+    body: JSON.stringify(query), // must match 'Content-Type' header
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'POST'
+  })
+  .then(response => response.json()) // parses response to JSON
 }
 
-function loadSermons(sermon_data_url) {
-  //Fetch the data and pass it to the function to draw it into the page.
-  fetch(sermon_data_url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(updatePageWithSermons)
-    .catch(function(rejection){
-      console.log(rejection);
-    });
+function getTalksFromGraphCool(serviceID) {
+  var graphCoolURL = `https://api.graph.cool/simple/v1/${serviceID}`;
+  console.log(graphCoolURL);
+  var query = {"query":
+  `query {
+    allSeries(first: 4) {
+      name
+      image3x2Url
+      sermons {
+        name
+        preachedAt
+        url
+        speakers {
+          name
+        }
+      }
+    }
+  }`};
+
+  queryGraphCool(graphCoolURL,query)
+  .then(updatePageWithSermons)
+  .catch(function(rejection){
+    console.log(rejection);
+  });
 }
 
+var serviceID = "cjhoh936q44gz0181840a6nlj"
 
 //Do things when the DOM content has loaded so that we can safely manipulate the DOM.
 document.addEventListener("DOMContentLoaded", function() {
   showPodcastLink();
-  loadSermons(sermon_data_url);
+  getTalksFromGraphCool(serviceID); 
 });
